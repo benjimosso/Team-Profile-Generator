7d264fa93596b0aa4f3dddd02ea938109d13cf06
benjimosso <benjimosso@gmail.com>
2021-06-05 21:44:15 -0700

    Employee js

---
+++ index.js
@@ -0,0 +1,12 @@
+// Dependencies
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const jest = require('jest');
+
+// Constructors
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Employee = require('./lib/Employee');
+const Intern = require('./lib/Intern')
